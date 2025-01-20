c5c2e2fc570e8b05b155cc6f305a62aee1e7dea1
Babatunde Taiwo <tbabson20@gmail.com>
2025-01-20 15:21:04 +0100

    Create CurrentUser.jsx
    current user file added

---
+++ client/src/components/CurrentUser.jsx
@@ -0,0 +1,12 @@
+import Wrapper from "../assets/wrappers/currentUser";
+
+const CurrentUser = () => {
+  return (
+    <Wrapper>
+      <div className="currentUser">
+        <p>welcome babatunde taiwo</p>
+      </div>
+    </Wrapper>
+  );
+};
+export default CurrentUser;
